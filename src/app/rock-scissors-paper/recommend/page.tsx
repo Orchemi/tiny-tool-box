fce8d67f64dc8ee15acbb5fd70b43e0a2e42ceba
박승훈 <tmdgns1126@naver.com>
2024-11-15 23:58:46 +0900

    Feat: 가위바위보 랜덤 추천 페이지 클릭시 변경되는 배경 추가

--- src/app/rock-scissors-paper/recommend/page.tsx
+++ src/app/rock-scissors-paper/recommend/page.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { twMerge } from "tw-merge";
 
 type RockScissorsPaper = "rock" | "scissors" | "paper";
 const choices: RockScissorsPaper[] = ["rock", "scissors", "paper"];
 
 export default function Page() {
   const [choice, setChoice] = useState<RockScissorsPaper>("rock");
+  const [color, setColor] = useState<string>("#000000");
 
-  const handleChangeChoice = () => {
+  const handleChangeChoice = useCallback(() => {
     const randomIndex = Math.floor(Math.random() * choices.length);
     const randomChoice = choices[randomIndex];
     setChoice(randomChoice);
+    setColor(getRandomColor());
+  }, []);
+
+  const getRandomColor = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomColor}`;
   };
 
   useEffect(() => {
     handleChangeChoice();
-  }, []);
+    setColor(getRandomColor());
+  }, [handleChangeChoice]);
 
   return (
     <main
@@ -25,16 +34,28 @@ export default function Page() {
     >
       <section
         className={
-          "flex flex-col gap-6 items-center w-full justify-center mobile:gap-20"
+          "flex flex-col gap-6 items-center w-full justify-center h-full mobile:gap-20"
         }
       >
-        <h1 className={"font-extrabold text-[24px]"}>가위바위보 랜덤 추천</h1>
+        <h1 className={"font-extrabold text-[20px] mobile:text-[24px] "}>
+          가위바위보 랜덤 추천
+        </h1>
         <div
           className={
-            "relative w-20 aspect-square flex justify-center items-center mobile:w-30"
+            "flex justify-center items-center rounded-full w-[120px] aspect-square mobile:w-[200px]"
           }
+          style={{
+            backgroundColor: `${color}20`,
+            border: `3px solid ${color}`,
+          }}
         >
-          <img src={`/imgs/${choice}.svg`} alt={"choice-img"} />
+          <div className={twMerge("relative flex justify-center items-center")}>
+            <img
+              src={`/imgs/${choice}.svg`}
+              alt={"choice-img"}
+              className={"w-[80px] aspect-square mobile:w-[120px]"}
+            />
+          </div>
         </div>
       </section>
     </main>
